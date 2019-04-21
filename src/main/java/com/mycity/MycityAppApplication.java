c3f4cc77f8709d1f227fd69bc2c41482ca2da2c4
EXT02D8158 <TYMvn72>
2019-04-21 23:12:44 +0300

    Initial commit

---
+++ src/main/java/com/mycity/MycityAppApplication.java
@@ -0,0 +1,13 @@
+package com.mycity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycityAppApplication.class, args);
+	}
+
+}
